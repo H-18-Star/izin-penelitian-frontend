1acabd24eabd725470dbfe89be4f3d050292a316
h-18-star <hermanjayadi1802@gmail.com>
2025-07-27 03:00:50 +0800

    Commit awal: proyek izin penelitian frontend

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FormIzinPenelitian from "./components/FormIzinPenelitian";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <FormIzinPenelitian />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FormIzinPenelitian.js
@@ -0,0 +1,1 @@
+<your previously provided code here>
