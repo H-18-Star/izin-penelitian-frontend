08aa992ed2599aca3a8d44b1f96f12e08b0b1dcf
h-18-star <hermanjayadi1802@gmail.com>
2025-07-27 11:29:40 +0800

    Fix: memperbaiki error import di FormIzinPenelitian.js

--- src/components/FormIzinPenelitian.js
+++ src/components/FormIzinPenelitian.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 function FormIzinPenelitian() {
   const [formData, setFormData] = useState({
